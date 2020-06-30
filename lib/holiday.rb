773a09f6fc9183d05d923789ca28aa6b6504f90f
Andrea Tchalaou <attchalaou@gmail.com>
2020-06-30 03:04:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,45 +1,45 @@
-require'pry'
+# require'pry'
 
-holiday_supplies = {
-  :winter => {
-    :christmas => ["Lights", "Wreath"],
-    :new_years => ["Party Hats"]
-  },
-  :summer => {
-    :fourth_of_july => ["Fireworks", "BBQ"]
-  },
-  :fall => {
-    :thanksgiving => ["Turkey"]
-  },
-  :spring => {
-    :memorial_day => ["BBQ"]
-  }
-}
-def second_supply_for_fourth_of_july(holiday_supplies)
-  holiday_hash[:summer][fourth_of_july][1]
-end
+# holiday_supplies = {
+#   :winter => {
+#     :christmas => ["Lights", "Wreath"],
+#     :new_years => ["Party Hats"]
+#   },
+#   :summer => {
+#     :fourth_of_july => ["Fireworks", "BBQ"]
+#   },
+#   :fall => {
+#     :thanksgiving => ["Turkey"]
+#   },
+#   :spring => {
+#     :memorial_day => ["BBQ"]
+#   }
+# }
+# def second_supply_for_fourth_of_july(holiday_supplies)
+#   holiday_hash[:summer][fourth_of_july][1]
+# end
   
 
-def all_supplies(holiday_supplies)
-  holiday_supplies.each do |season, data|
-    holiday.to_s.split("_").collect {|word| word.capitalize!}.join(" ")
+# def all_supplies(holiday_supplies)
+#   holiday_supplies.each do |season, data|
+#     holiday.to_s.split("_").collect {|word| word.capitalize!}.join(" ")
       
-      puts "  #{str_holiday}: #{supply.join(", ")}"
-    end
-  end 
-end 
+#       puts "  #{str_holiday}: #{supply.join(", ")}"
+#     end
+#   end 
+# end 
 
-all_supplies(holiday_supplies)
+# all_supplies(holiday_supplies)
 
-def all_holidays_with_bbq(holiday_hash)
+# def all_holidays_with_bbq(holiday_hash)
   
-  holiday_hash.map do |season, holidays|
-    holidays.map do |holiday, supplies|
-      holiday if supplies.include?("BBQ")
-    end
-  end.flatten.compact
-end
-all_holidays_with_bbq(holiday_supplies)
+#   holiday_hash.map do |season, holidays|
+#     holidays.map do |holiday, supplies|
+#       holiday if supplies.include?("BBQ")
+#     end
+#   end.flatten.compact
+# end
+# all_holidays_with_bbq(holiday_supplies)
 
 require 'pry'
 def second_supply_for_fourth_of_july(holiday_hash)
